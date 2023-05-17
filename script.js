901a92e226c7509eb4880260f40a7190f012a729
Minskoleopc <minskoleopc@gmail.com>
2023-05-17 06:15:21 +0530

    added

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let headOne = document.querySelector('h1')
+headOne.addEventListener('click',function(){
+    headOne.textContent =   headOne.textContent.toUpperCase()
+    headOne.style.color = "green"
+})
